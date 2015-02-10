2a122bcf5fdb080fa2484a62dad6157dedb3bcdd
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-10 08:15:48 -0500

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install -y python-matplotlib python-pandas
+
 # for F in training.zip test.zip IdLookupTable.csv SampleSubmission.csv;
 # do
 #     scp ubuntu@ddbolineinthecloud.mooo.com:~/setup_files/build/kaggle_facial_keypoints/$F .
@@ -12,3 +14,5 @@
 ./plot_net2_training_loss.py
 ./convolutions_flip.py
 ./plot_net3_training_loss.py
+
+scp *.png *.pickle ddboline@ddbolineathome.mooo.com:~/setup_files/build/kaggle_facial_keypoints/ 

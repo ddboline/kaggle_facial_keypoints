60994cb2ff2417198bd7f0d62c899b7f4fa7c67c
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-10 07:41:28 -0500

    ...

--- plot_net3_training_loss.py
+++ plot_net3_training_loss.py
@@ -17,6 +17,9 @@ with open('net1.pickle', 'rb') as f:
 with open('net2.pickle', 'rb') as f:
     net2 = pickle.load(f)
 
+with open('net3.pickle', 'rb') as f:
+    net3 = pickle.load(f)
+
 train_loss1 = np.array([i["train_loss"] for i in net1.train_history_])
 valid_loss1 = np.array([i["valid_loss"] for i in net1.train_history_])
 pl.plot(train_loss1, linewidth=3, label="net1 train")
@@ -26,11 +29,17 @@ train_loss2 = np.array([i["train_loss"] for i in net2.train_history_])
 valid_loss2 = np.array([i["valid_loss"] for i in net2.train_history_])
 pl.plot(train_loss2, linewidth=3, label="net2 train")
 pl.plot(valid_loss2, linewidth=3, label="net2 valid")
+
+train_loss3 = np.array([i["train_loss"] for i in net3.train_history_])
+valid_loss3 = np.array([i["valid_loss"] for i in net3.train_history_])
+pl.plot(train_loss3, linewidth=3, label="net3 train")
+pl.plot(valid_loss3, linewidth=3, label="net3 valid")
+
 pl.grid()
 pl.legend()
 pl.xlabel("epoch")
 pl.ylabel("loss")
 pl.ylim(1e-3, 1e-2)
 pl.yscale("log")
-pl.savefig('training_loss_net2.png')
+pl.savefig('training_loss_net3.png')
 
--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-for F in training.zip test.zip IdLookupTable.csv SampleSubmission.csv;
-do
-    scp ubuntu@ddbolineinthecloud.mooo.com:~/setup_files/build/kaggle_facial_keypoints/$F .
-done
+# for F in training.zip test.zip IdLookupTable.csv SampleSubmission.csv;
+# do
+#     scp ubuntu@ddbolineinthecloud.mooo.com:~/setup_files/build/kaggle_facial_keypoints/$F .
+# done
 
+./test.py
+./single_hidden_layer.py
+./plot_net1_training_loss.py
+./convolutions.py
+./plot_net2_training_loss.py
+./convolutions_flip.py
+./plot_net3_training_loss.py

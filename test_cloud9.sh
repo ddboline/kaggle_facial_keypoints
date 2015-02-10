944656644b69b2e10399300e745a954ef2a4f10f
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-09 20:30:37 -0500

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for F in training.zip test.zip IdLookupTable.csv SampleSubmission.csv;
+for F in "training.zip test.zip IdLookupTable.csv SampleSubmission.csv";
 do
     scp ubuntu@ddbolineinthecloud.mooo.com:~/$F .
 done

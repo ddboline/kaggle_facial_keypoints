f0b313f9081bdf1a9054b80167b7a55128f23943
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-09 20:32:58 -0500

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for F in "training.zip test.zip IdLookupTable.csv SampleSubmission.csv";
+for F in training.zip test.zip IdLookupTable.csv SampleSubmission.csv;
 do
     scp ubuntu@ddbolineinthecloud.mooo.com:~/setup_files/build/kaggle_facial_keypoints/$F .
 done

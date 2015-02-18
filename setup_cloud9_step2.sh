1662dc7883809c92703e05d2ceb6aaa79f3347ee
Daniel Boline <ddboline@gmail.com>
2015-02-18 16:16:55 -0500

    fixes

--- setup_cloud9_step2.sh
+++ setup_cloud9_step2.sh
@@ -12,10 +12,12 @@ source venv/bin/activate
 # pip install -r requirements.txt
 # pip install -r requirements-2.txt
 
-pip install --upgrade theano 2>&1 > theano_build.log
-pip install git+https://github.com/benanne/Lasagne.git 2>&1 > lasagne_build.log
-pip install nolearn 2>&1 > nolearn_build.log
-pip install git+https://github.com/lisa-lab/pylearn2.git 2>&1 > pylearn2_build.log
+### getting this to work may require:
+### installing from https://github.com/Theano/Theano.git
+### changing a line in theano/sandbox/cuda/cuda_ndarray.cuh (more likely the former than the latter)
+sudo pip install --upgrade theano 2>&1 > theano_build.log
+sudo pip install git+https://github.com/benanne/Lasagne.git 2>&1 > lasagne_build.log
+sudo pip install nolearn 2>&1 > nolearn_build.log
 
 for F in training.zip test.zip IdLookupTable.csv SampleSubmission.csv;
 do
@@ -26,3 +28,9 @@ for F in training.zip test.zip;
 do
     unzip -x $F;
 done
+
+cd $HOME
+git clone pip install https://github.com/lisa-lab/pylearn2.git
+### this doesn't really work any other way
+cd pylearn2
+echo "You will need to build pylearn2 manually (setup.py written by idiot...)"

34924a8ddac95c3d9fe332fb1cf72f6f68384aaf
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-18 22:09:25 -0500

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -4,27 +4,28 @@ sudo modprobe nvidia-340
 sudo modprobe nvidia-340-uvm
 
 touch output.out
+touch output.err
 if [ -z $1 ]; then
-    ./test.py 2>&1 >> output.out
-    ./net1_single_hidden_layer.py 2>&1 >> output.out
-    ./plot_net1_training_loss.py 2>&1 >> output.out
-    ./net2_convolutions.py 2>&1 >> output.out
-    ./plot_net2_training_loss.py 2>&1 >> output.out
+    ./test.py >> output.out 2>> output.err
+    ./net1_single_hidden_layer.py >> output.out 2>> output.err
+    ./plot_net1_training_loss.py >> output.out 2>> output.err
+    ./net2_convolutions.py >> output.out 2>> output.err
+    ./plot_net2_training_loss.py >> output.out 2>> output.err
 elif [ $1 = "2" ]; then
-    ./net3_convolutions_flip.py 2>&1 >> output.out
-    ./plot_net3_training_loss.py 2>&1 >> output.out
-    ./net4_update_learning_rate.py 2>&1 >> output.out
-    ./plot_net3_training_loss.py 2>&1 >> output.out
+    ./net3_convolutions_flip.py >> output.out 2>> output.err
+    ./plot_net3_training_loss.py >> output.out 2>> output.err
+    ./net4_update_learning_rate.py >> output.out 2>> output.err
+    ./plot_net3_training_loss.py >> output.out 2>> output.err
 elif [ $1 = "3" ]; then
-    ./net5_update_learning_rate_flipped.py 2>&1 >> output.out
-    ./plot_net3_training_loss.py 2>&1 >> output.out
-    ./net6_dropout.py 2>&1 >> output.out
-    ./plot_net3_training_loss.py 2>&1 >> output.out
+    ./net5_update_learning_rate_flipped.py >> output.out 2>> output.err
+    ./plot_net3_training_loss.py >> output.out 2>> output.err
+    ./net6_dropout.py >> output.out 2>> output.err
+    ./plot_net3_training_loss.py >> output.out 2>> output.err
 elif [ $1 = "4" ]; then
-    ./net7_bigger.py 2>&1 >> output.out
-    ./plot_net3_training_loss.py 2>&1 >> output.out
-    ./net8_moar_epochs.py 2>&1 >> output.out
-    ./plot_net3_training_loss.py 2>&1 >> output.out
+    ./net7_bigger.py >> output.out 2>> output.err
+    ./plot_net3_training_loss.py >> output.out 2>> output.err
+    ./net8_moar_epochs.py >> output.out 2>> output.err
+    ./plot_net3_training_loss.py >> output.out 2>> output.err
 fi
 
 if [ -z $1 ]; then
@@ -33,6 +34,6 @@ else
     D=$1_`date +%Y%m%d%H%M%S`
 fi
 ssh ddboline@ddbolineathome.mooo.com "mkdir -p ~/setup_files/build/kaggle_facial_keypoints/output_${D}"
-scp output.out *.png *.pickle ddboline@ddbolineathome.mooo.com:~/setup_files/build/kaggle_facial_keypoints/output_${D}
+scp output.out output.err *.png *.pickle ddboline@ddbolineathome.mooo.com:~/setup_files/build/kaggle_facial_keypoints/output_${D}
 ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk DONE_${D}"
 sudo shutdown now
